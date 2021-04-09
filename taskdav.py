feec2b1e5bef442353133490357644afd04a5fc3
Richard Hibbitts <richard.hibbitts@vitalsource.com>
2021-04-09 16:45:43 -0400

    Make it a real script

--- taskdav.py
+++ taskdav.py
@@ -1,26 +1,31 @@
 from datetime import datetime
 import os
 import caldav
+from task import Task
 
 ## CONFIGURATION.
 caldav_url = os.environ.get('CALDAV_URL')
 username = os.environ.get('TASK_USERNAME')
 password = os.environ.get('TASK_PWORD')
 
-client = caldav.DAVClient(url=caldav_url, username=username, password=password)
+def main():
+    client = caldav.DAVClient(url=caldav_url, username=username, password=password)
 
-## Typically the next step is to fetch a principal object.
-## This will cause communication with the server.
-my_principal = client.principal()
+    ## Typically the next step is to fetch a principal object.
+    ## This will cause communication with the server.
+    my_principal = client.principal()
 
-## The principals calendars can be fetched like this:
-calendars = my_principal.calendars()
+    ## The principals calendars can be fetched like this:
+    calendars = my_principal.calendars()
 
-## Fetch the tasks
-todos = calendars[1].todos()
+    ## Fetch the tasks
+    todos = calendars[1].todos()
 
-for t in todos:
-    print(t.data)
+    for t in todos:
+        task = Task(t.data)
+        print(task.serialize())
 
 ## It's possible to fetch historic tasks too
 ##todos = my_new_tasklist.todos(include_completed=True)
+if __name__ == "__main__":
+    main()
