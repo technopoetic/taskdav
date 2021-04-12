3a2873082c0086bf3a30fe5e2689219642e4f559
Richard Hibbitts <richard@richardhibbitts.com>
2021-04-12 16:39:07 -0400

    Black formatting

--- task.py
+++ task.py
@@ -1,7 +1,8 @@
 import vobject
 import json
 
-class Task():
+
+class Task:
     def __init__(self, vdata):
         self.data_dict = vobject.readOne(vdata)
 
@@ -19,13 +20,13 @@ class Task():
     @property
     def to_dict(self):
         return {
-            'categories': self.categories,
-            'class': self.task_class,
-            'created': self.created,
-            'description': self.description,
-            'priority': self.priority,
-            'summary': self.summary,
-            'status': self.status
+            "categories": self.categories,
+            "class": self.task_class,
+            "created": self.created,
+            "description": self.description,
+            "priority": self.priority,
+            "summary": self.summary,
+            "status": self.status,
         }
 
     def serialize(self):
@@ -33,19 +34,19 @@ class Task():
 
     def _parse_categories(self):
         categories = []
-        todo = self.data_dict.contents.get('vtodo')[0]
-        if todo.contents.get('categories') is not None:
-            for y in todo.contents.get('categories'):
+        todo = self.data_dict.contents.get("vtodo")[0]
+        if todo.contents.get("categories") is not None:
+            for y in todo.contents.get("categories"):
                 categories.append(y.value)
         return [item for sublist in categories for item in sublist]
 
     def _parse_summary(self):
-        todo = self.data_dict.contents.get('vtodo')[0]
-        return todo.contents.get('summary')[0].value
+        todo = self.data_dict.contents.get("vtodo")[0]
+        return todo.contents.get("summary")[0].value
 
     def _parse_description(self):
-        todo = self.data_dict.contents.get('vtodo')[0]
-        desc_list = todo.contents.get('description')
+        todo = self.data_dict.contents.get("vtodo")[0]
+        desc_list = todo.contents.get("description")
 
         if desc_list is not None:
             return desc_list[0].value
@@ -53,33 +54,33 @@ class Task():
             return None
 
     def _parse_status(self):
-        todo = self.data_dict.contents.get('vtodo')[0]
-        status = todo.contents.get('status')
+        todo = self.data_dict.contents.get("vtodo")[0]
+        status = todo.contents.get("status")
         if status is not None:
             return status[0].value
-        else: 
+        else:
             return None
 
     def _parse_class(self):
-        todo = self.data_dict.contents.get('vtodo')[0]
-        task_class = todo.contents.get('class')
+        todo = self.data_dict.contents.get("vtodo")[0]
+        task_class = todo.contents.get("class")
         if task_class is not None:
-            return task_class[0].value 
+            return task_class[0].value
         else:
             return None
 
     def _parse_priority(self):
-        todo = self.data_dict.contents.get('vtodo')[0]
-        priority = todo.contents.get('priority')
+        todo = self.data_dict.contents.get("vtodo")[0]
+        priority = todo.contents.get("priority")
         if priority is not None:
-            return priority[0].value 
+            return priority[0].value
         else:
             return None
 
     def _parse_created(self):
-        todo = self.data_dict.contents.get('vtodo')[0]
-        created = todo.contents.get('created')
+        todo = self.data_dict.contents.get("vtodo")[0]
+        created = todo.contents.get("created")
         if created is not None:
-            return created[0].value 
+            return created[0].value
         else:
             return None
