3d5771c1a4e257041ed36de777df3f7d3a814056
Richard Hibbitts <richard@richardhibbitts.com>
2021-04-12 16:39:24 -0400

    Integrate click

--- taskdav.py
+++ taskdav.py
@@ -1,31 +1,62 @@
+#!/usr/bin/env python
+
 from datetime import datetime
+import click
 import os
 import caldav
 from task import Task
 
 ## CONFIGURATION.
-caldav_url = os.environ.get('CALDAV_URL')
-username = os.environ.get('TASK_USERNAME')
-password = os.environ.get('TASK_PWORD')
+caldav_url = os.environ.get("CALDAV_URL")
+task_calendar = os.environ.get("TASK_CALENDAR")
+username = os.environ.get("TASK_USERNAME")
+password = os.environ.get("TASK_PWORD")
 
-def main():
+
+def get_tasks(include_completed=False):
     client = caldav.DAVClient(url=caldav_url, username=username, password=password)
-
-    ## Typically the next step is to fetch a principal object.
-    ## This will cause communication with the server.
     my_principal = client.principal()
-
-    ## The principals calendars can be fetched like this:
     calendars = my_principal.calendars()
+    task_cal = None
+    for cal in calendars:
+        if cal.name == task_calendar:
+            task_cal = cal
 
-    ## Fetch the tasks
-    todos = calendars[1].todos()
+    if task_cal is not None:
+        todos = task_cal.todos(include_completed=include_completed)
+        return todos
+    else:
+        raise ValueError('No Calendar specified for Tasks.')
 
-    for t in todos:
-        task = Task(t.data)
-        print(task.serialize())
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.option(
+    '--include-completed',
+    '-i',
+    is_flag=True,
+    help='Include completed Tasks in the task list.')
+def list(include_completed):
+    try:
+        todos = get_tasks(include_completed)
+
+        for t in todos:
+            task = Task(t.data)
+            print(task.serialize())
+    except ValueError as e:
+        print(e)
+
+@click.command()
+def create():
+    print("Not implemented yet")
+
+cli.add_command(list)
+cli.add_command(create)
 
 ## It's possible to fetch historic tasks too
 ##todos = my_new_tasklist.todos(include_completed=True)
 if __name__ == "__main__":
-    main()
+    cli()
