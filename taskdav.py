13b8add9cb1d0b2f0fd8fbe95346780c17eabc04
Richard Hibbitts <richard@richardhibbitts.com>
2021-04-13 15:14:01 -0400

    Create a task

--- taskdav.py
+++ taskdav.py
@@ -14,20 +14,30 @@ password = os.environ.get("TASK_PWORD")
 
 
 def get_tasks(include_completed=False):
-    client = caldav.DAVClient(url=caldav_url, username=username, password=password)
-    my_principal = client.principal()
-    calendars = my_principal.calendars()
-    task_cal = None
-    for cal in calendars:
-        if cal.name == task_calendar:
-            task_cal = cal
-
+    task_cal = get_task_cal()
     if task_cal is not None:
         todos = task_cal.todos(include_completed=include_completed)
         return todos
     else:
         raise ValueError("No Calendar specified for Tasks.")
 
+def create_task(data={}):
+    task_cal = get_task_cal()
+    vdata = (
+    f'BEGIN:VCALENDAR\n'
+    f'BEGIN:VTODO\n'
+    f'SUMMARY:{data.get("summary")}\n'
+    f'CATEGORIES:{data.get("category")}\n'
+    f'STATUS:{data.get("status")}\n'
+    f'PRIORITY:{data.get("priority")}\n'
+    f'END:VTODO\n'
+    f'END:VCALENDAR'
+    )
+
+    if task_cal is not None:
+        task_cal.save_todo(vdata)
+    else:
+        raise ValueError("No Calendar specified for Tasks.")
 
 @click.group()
 def cli():
@@ -63,9 +73,34 @@ def list(include_completed):
     #                    / "COMPLETED"    ;Indicates to-do completed.
     #                    / "IN-PROCESS"   ;Indicates to-do in process of.
     #                    / "CANCELLED"
-def create():
-    print("Not implemented yet")
+def create(category=None, access=None, desc=None, priority=None, summary=None, status=None):
+    try:
+        create_task({
+            "category": category,
+            "access": access,
+            "description": desc,
+            "priority": priority,
+            "summary": summary,
+            "status": status
+        })
+        todos = get_tasks()
 
+        for t in todos:
+            task = Task(t.data)
+            print(task.pp())
+            print(task.serialize())
+    except ValueError as e:
+        print(e)
+
+def get_task_cal():
+    client = caldav.DAVClient(url=caldav_url, username=username, password=password)
+    my_principal = client.principal()
+    calendars = my_principal.calendars()
+    task_cal = None
+    for cal in calendars:
+        if cal.name == task_calendar:
+            task_cal = cal
+    return task_cal
 
 cli.add_command(list)
 cli.add_command(create)
